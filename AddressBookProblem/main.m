ac2a8eb3246feb5cb1573a1f7e34b57d3a24f565
davidBear <davidBear@softbank126126176052.bbtec.net>
2011-09-04 03:15:01 +0900

    Initial Commit

---
+++ AddressBookProblem/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AddressBookProblem
+//
+//  Created by davidBear on 11-9-4.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
